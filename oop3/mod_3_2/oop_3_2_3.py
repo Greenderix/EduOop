25a0eee52ec284e523045fa60ad2f577c29c31ee
Evgeny <grinderix@yandex.ru>
2023-03-07 01:46:15 +0300

    upd

--- oop3/mod_3_2/oop_3_2_3.py
+++ oop3/mod_3_2/oop_3_2_3.py
@@ -1,2 +1,16 @@
 class ImageFileAcceptor:
-    ...
+    """
+    Проверка расширений файлов
+    """
+    def __init__(self, extensions):
+        self.extensions = extensions
+    def __call__(self, name, *args, **kwargs):
+        start = name.rfind('.')
+        ext = "" if start == -1 else name[start+1:]
+        return ext in self.extensions
+
+
+filenames = ["boat.jpg", "web.png", "text.txt", "python.doc", "ava.jpg", "forest.jpeg", "eq_1.png", "eq_2.png"]
+acceptor = ImageFileAcceptor(('jpg', 'bmp', 'jpeg'))
+image_filenames = filter(acceptor, filenames)
+print(list(image_filenames))  # ["boat.jpg", "ava.jpg", "forest.jpeg"]
